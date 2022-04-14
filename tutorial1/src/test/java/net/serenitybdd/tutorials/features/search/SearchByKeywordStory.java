5de09dd802c3f061c0fa8cdddddc310ff404fa92
gallardo <argrico@gmail.com>
2022-04-14 17:37:59 +0200

    Switch library browse-the-web -> serenity-screenplay-webdriver

--- tutorial1/src/test/java/net/serenitybdd/tutorials/features/search/SearchByKeywordStory.java
+++ tutorial1/src/test/java/net/serenitybdd/tutorials/features/search/SearchByKeywordStory.java
@@ -4,20 +4,22 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.questions.page.TheWebPage;
-import net.thucydides.core.annotations.Issue;
+import net.serenitybdd.tutorials.tasks.OpenTheApplication;
+import net.serenitybdd.tutorials.tasks.Search;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import net.serenitybdd.tutorials.tasks.OpenTheApplication;
-import net.serenitybdd.tutorials.tasks.Search;
 
-import static net.serenitybdd.screenplay.GivenWhenThen.*;
 import static net.serenitybdd.screenplay.EventualConsequence.eventually;
+import static net.serenitybdd.screenplay.GivenWhenThen.givenThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+import static net.serenitybdd.screenplay.GivenWhenThen.then;
+import static net.serenitybdd.screenplay.GivenWhenThen.when;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.hasItem;
 
 @RunWith(SerenityRunner.class)
 public class SearchByKeywordStory {
